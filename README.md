fc125efc01725d15a06cd37a35b4198d6ccba7a1
Felix Angelov <felix@shorebird.dev>
2024-09-23 14:02:57 -0500

    chore: move to script

--- README.md
+++
@@ -1,3 +0,0 @@
-# flutter_install_apk_bug
-
-A new Flutter project.
---
+++ scripts/ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+# Create a temporary directory
+TEMP_DIR=$(mktemp -d -t 'workspace-XXXXX')
+cd "$TEMP_DIR"
+
+# Create a new empty flutter project
+flutter create e2e_test --empty --platforms android
+cd e2e_test
+
+# Replace the contents of "lib/main.dart" with a single print statement.
+echo "void main() { print('hello world'); }" >lib/main.dart
+
+# Create a new release on Android
+flutter build appbundle --release
+
+# Download bundletool 1.17.1
+curl -L -o bundletool.jar https://github.com/google/bundletool/releases/download/1.17.1/bundletool-all-1.17.1.jar
+
+# Build a universal APK
+java -jar bundletool.jar build-apks --overwrite --bundle=build/app/outputs/bundle/release/app-release.aab --output=outputs/my_app.apks --mode=universal
+
+# Install the APK on a device
+java -jar bundletool.jar install-apks --allow-downgrade --apks=outputs/my_app.apks
